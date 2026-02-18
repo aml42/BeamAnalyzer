Fixed an issue where the deflection calculation was wrong by a factor of 1000 due to a mm to m conversation falsely applied

src/loadcalculator/deflection_calculator.py
@@ -134,7 +134,7 @@ class DeflectionCalculator:
         """Return the calculated deflection values."""
         if self._deflection is None:
             self._calculate_deflection()
-        return self._deflection * 1000.0
+        return self._deflection.copy()
     
     @property
     def slope(self) -> np.ndarray: